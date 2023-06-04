5318a1be7a9d13cf214cee889283974af5b1846b
Aarogya Yadav <aky104@student.bham.ac.uk>
2023-06-04 12:57:57 +0100

    Block motion

--- cube wave.py
+++ cube wave.py
@@ -5,23 +5,25 @@ pygame.init()
 width = (800,800)
 win = pygame.display.set_mode(width)
 clock = pygame.time.Clock()
-f1Colour = (255,255,255)
-f2Colour = (190,190,190)
-topColour = (100,100,100)
-offset = -10
+topColour = (255,255,255)
+f1Colour = (190,190,190)
+f2Colour = (100,100,100)
+offset = -7
+height = 14*9
+width = 14
 
 class Bar:
     def __init__(self, x, y, start):
         self.pos = [x,y]
-        self.start = start
-        self.width = 20
-        self.height = 30
-        self.addedHeight = 100
+        self.addedHeight = 0
+        self.time = start
 
     def draw(self):
-        pygame.draw.polygon(win, f1Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+self.width, self.pos[1]+offset-self.addedHeight], [self.pos[0]+self.width, self.pos[1]+self.height+offset+self.addedHeight], [self.pos[0], self.pos[1]+self.height+self.addedHeight]])
-        pygame.draw.polygon(win, f2Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]-self.width, self.pos[1]+offset-self.addedHeight], [self.pos[0]-self.width, self.pos[1]+self.height+offset+self.addedHeight], [self.pos[0], self.pos[1]+self.height+self.addedHeight]])
-        pygame.draw.polygon(win, topColour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+self.width, self.pos[1]+offset-self.addedHeight], [self.pos[0], self.pos[1]+2*offset-self.addedHeight], [self.pos[0]-self.width, self.pos[1]+offset-self.addedHeight]])
+        self.time = (self.time + 2) %360
+        self.addedHeight = math.sin(math.radians(self.time))*(height/3)
+        pygame.draw.polygon(win, f1Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+width, self.pos[1]+offset-self.addedHeight], [self.pos[0]+width, self.pos[1]+height+offset+self.addedHeight], [self.pos[0], self.pos[1]+height+self.addedHeight]])
+        pygame.draw.polygon(win, f2Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]-width, self.pos[1]+offset-self.addedHeight], [self.pos[0]-width, self.pos[1]+height+offset+self.addedHeight], [self.pos[0], self.pos[1]+height+self.addedHeight]])
+        pygame.draw.polygon(win, topColour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+width, self.pos[1]+offset-self.addedHeight], [self.pos[0], self.pos[1]+2*offset-self.addedHeight], [self.pos[0]-width, self.pos[1]+offset-self.addedHeight]])
 
 
 
@@ -30,11 +32,27 @@ class Bar:
 
 def redrawWin():
     win.fill((0,0,0))
-    bar1.draw()
+    
+    for bar in cube:
+        bar.draw()
+    pygame.draw.circle(win,(255,0,0),(400,400),5)
     pygame.display.update()
     
 
-bar1 = Bar(400,400,0)
+cube = []
+n = 20
+yMove = 280
+xMove = 400
+for i in range(n):
+    for j in range(i+1):
+        offsetX = j*(width*2) - (i*width)
+        cube.append(Bar(xMove+offsetX,yMove+i*(offset*-1),0))
+
+for i in range(n-1):
+    for j in range((n-1)-i):
+        offsetX = j*(width*2) - (((n-1)-i)*width) + width
+        cube.append(Bar(xMove+offsetX,yMove+((n-1)*offset*-1)+i*(offset*-1)-offset,0))
+
 
 run = True
 while run:
