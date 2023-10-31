8d62b351686d67d623540713f6460477fefd8784
Aarogya Yadav <aky104@student.bham.ac.uk>
2023-10-31 18:50:47 +0000

    centered and cleaned code

--- cube wave.py
+++ cube wave.py
@@ -8,9 +8,10 @@ clock = pygame.time.Clock()
 topColour = (255,255,255)
 f1Colour = (190,190,190)
 f2Colour = (100,100,100)
-offset = -7
-height = 14*9
-width = 14
+offset = -10
+wide = offset*-2
+height = wide*9
+
 
 class Bar:
     #initialising the bar starting coordinates, the added height they will have added on which will be used to display motion and time which is used to move that height every frame
@@ -20,14 +21,16 @@ class Bar:
         self.time = start
         self.column = column
         self.row = row
+        self.scale = wide*3
 
     # the draw function draws 3 polygon faces that represent the 3 visible faces of each bar, the time of each bar is incremented to function as an angle and the added height is adjusted appropriately
     def draw(self):
         self.time = (self.time + 2) %360
-        self.addedHeight = math.cos(math.radians(self.time))*(height/3)
-        pygame.draw.polygon(win, f1Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+width, self.pos[1]+offset-self.addedHeight], [self.pos[0]+width, self.pos[1]+height+offset+self.addedHeight], [self.pos[0], self.pos[1]+height+self.addedHeight]])
-        pygame.draw.polygon(win, f2Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]-width, self.pos[1]+offset-self.addedHeight], [self.pos[0]-width, self.pos[1]+height+offset+self.addedHeight], [self.pos[0], self.pos[1]+height+self.addedHeight]])
-        pygame.draw.polygon(win, topColour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+width, self.pos[1]+offset-self.addedHeight], [self.pos[0], self.pos[1]+2*offset-self.addedHeight], [self.pos[0]-width, self.pos[1]+offset-self.addedHeight]])
+        self.addedHeight = math.cos(math.radians(self.time))*(self.scale)
+        
+        pygame.draw.polygon(win, f1Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+wide, self.pos[1]+offset-self.addedHeight], [self.pos[0]+wide, self.pos[1]+height+offset+self.addedHeight], [self.pos[0], self.pos[1]+height+self.addedHeight]])
+        pygame.draw.polygon(win, f2Colour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]-wide, self.pos[1]+offset-self.addedHeight], [self.pos[0]-wide, self.pos[1]+height+offset+self.addedHeight], [self.pos[0], self.pos[1]+height+self.addedHeight]])
+        pygame.draw.polygon(win, topColour, [[self.pos[0], self.pos[1]-self.addedHeight], [self.pos[0]+wide, self.pos[1]+offset-self.addedHeight], [self.pos[0], self.pos[1]+2*offset-self.addedHeight], [self.pos[0]-wide, self.pos[1]+offset-self.addedHeight]])
 
         #pygame.draw.circle(win, (0,255,0), self.pos,3) #debug
 
@@ -43,35 +46,37 @@ def redrawWin():
         for bar in section:
             bar.draw()
 
-    #pygame.draw.circle(win, (255,0,0), (400-14,280+14*(n/2)),3) #debug
+    #pygame.draw.circle(win, (255,0,0), (400-wide,280+wide*(n/2)),1) #debug
     pygame.display.update()
     
 
 cube = []
 n = 20
-yMove = 280
-xMove = 400
+yOr = 180
+yMove = yOr
+xOr = 420
+xMove = xOr
 
 time = 0
 
 # for i in range(n):
 #     temp = []
-#     xMove -= width          #change the sign of this operation and xPos operation to generate from right to left or vice versa
+#     xMove -= wide          #change the sign of this operation and xPos operation to generate from right to left or vice versa
 #     yMove -= offset         #change the sign of this operation and yPos operation to generate from bottom to top or vice versa
 #     for j in range(n):
-#         xPos = xMove + width*(j)
+#         xPos = xMove + wide*(j)
 #         yPos = yMove - j*(offset)
 #         temp.append(Bar(xPos, yPos, time+j+i, i, j))
 #     cube.append(temp)
 
 for i in range(n):
     temp = []
-    xMove -= width          #change the sign of this operation and xPos operation to generate from right to left or vice versa
+    xMove -= wide          #change the sign of this operation and xPos operation to generate from right to left or vice versa
     yMove -= offset         #change the sign of this operation and yPos operation to generate from bottom to top or vice versa
     for j in range(n):
-        xPos = xMove + width*(j)
+        xPos = xMove + wide*(j)
         yPos = yMove - j*(offset)
-        temp.append(Bar(xPos, yPos, math.dist((xPos,yPos),(400-14,280+14*(n/2))), i, j))
+        temp.append(Bar(xPos, yPos, -math.dist((xPos,yPos),(xOr-wide,yOr+wide*(n/2))), i, j))
     cube.append(temp)
 
 
@@ -101,7 +106,7 @@ while run:
     # framerate of 60 frames per second
     clock.tick(60)
 
-
+    
     #event check if user has closed window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
